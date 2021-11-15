ca0a91ddd0b7eed575c1e5a428b6510536cad6e5
Kazuhiro Watanabe <ralirulego103@gmail.com>
2021-11-15 15:24:46 +0900

    Created sample web api

---
+++ src/Services/Catalog/Catalog.API/Entities/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Entities.Repositories
+{
+    public interface IProductRepository
+    {
+         Task<IEnumerable<Product>> GetProductsAsync();
+         Task<Product> GetProductByIdAsync(string id);
+         Task<IEnumerable<Product>> GetProductByNameAsync(string name);
+         Task<IEnumerable<Product>> GetProductByCategoryAsync(string categoryName);
+         Task CreateProductAsync(Product product);
+         Task<bool> UpdateProductAsync(Product product);
+         Task<bool> DeleteProductAsync(string id);
+    }
+}
